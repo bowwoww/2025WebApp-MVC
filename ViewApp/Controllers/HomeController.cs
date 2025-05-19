8e9c064b87e2b44a5e7eaaf47e67348a8778bc1e
Bowwow <rabbit88823@gmail.com>
2025-05-19 16:32:31 +0800

    新增專案夜市(使用model)

---
+++ ViewApp/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging;
+using ViewApp.Models;
+
+namespace MyView.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //用陣列來存放白板上面的資料
+            string[] id = { "A01", "A02", "A03", "A04", "A05", "A06", "A07" };
+            string[] name = { "瑞豐夜市", "新堀江商圈", "六合夜市", "青年夜市", "花園夜市", "大東夜市", "武聖夜市" };
+
+            string[] address = { "813高雄市左營區裕誠路", "800高雄市新興區玉衡里", "800台灣高雄市新興區六合二路",
+                "80652高雄市前鎮區凱旋四路758號", "台南市北區海安路三段533號", "台南市東區林森路一段276號",
+                "台南市中西區武聖路69巷42號" };
+
+            //採用List模式可以隨時增加或刪除
+            List<NightMarket> list= new List<NightMarket>();
+            //採用陣列模式將固定陣列大小無法後續修改
+            //NightMarket[] list = new NightMarket[id.Length];
+
+            for (int i = 0; i < id.Length; i++)
+            {
+                //list模式
+                NightMarket nm = new NightMarket();
+                nm.Id = id[i];
+                nm.Name = name[i];
+                nm.Address = address[i];
+                list.Add(nm);
+
+                //陣列模式
+                //list[i] = new NightMarket
+                //{
+                //    Id = id[i],
+                //    Name = name[i],
+                //    Address = address[i]
+                //};
+            }
+
+
+            return View(list);
+
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ViewApp/Models/NightMarket.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewApp.Models
+{
+    public class NightMarket
+    {
+        [Display(Name="編號")]
+        [Required(ErrorMessage = "請輸入編號")]
+        [RegularExpression("A[0-9]{2}", ErrorMessage = "編號格式錯誤，請輸入大寫A加兩位數字")]
+
+        public string Id { get; set; } = null!;
+
+        [Display(Name = "名稱")]
+        [Required(ErrorMessage = "請輸入名稱")]
+        public string Name { get; set; } = null!;
+
+
+        [Display(Name = "地址")]
+        [Required(ErrorMessage = "請輸入地址")]
+        [StringLength(30, ErrorMessage = "地址長度不能超過30個字")]
+        public string Address { get; set; } = null!;
+    }
+}
