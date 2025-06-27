8f99eee01a414e38d888a8bfb6019139a2f80d59
Bowwow <rabbit88823@gmail.com>
2025-06-27 14:13:55 +0800

    直接增加可供使用api : CategoryApi() 和 ProductApi(int? categoryId)

--- Northwind_DBFirst/Controllers/VMProducts.cs
+++ Northwind_DBFirst/Controllers/VMProducts.cs
@@ -33,4 +33,36 @@ public class ProductCategoryController : Controller
             .ToList();
         return PartialView("ProductTable", prod);
     }
+
+    public IActionResult ProductApi(int? categoryId)
+    {
+
+        //不要傳入picture
+        var products = _context.Products
+            .Where(p => p.CategoryId == categoryId)
+            .Include(p => p.Category)
+            .Select(p => new
+            {
+                p.ProductId,
+                p.ProductName,
+                p.UnitPrice,
+                p.UnitsInStock,
+                CategoryName = p.Category.CategoryName,
+                p.Discontinued
+            })
+            .ToList();
+        return Json(products);
+    }
+
+    public IActionResult CategoryApi()
+    {
+        var categories = _context.Categories
+            .Select(c => new
+            {
+                c.CategoryId,
+                c.CategoryName
+            })
+            .ToList();
+        return Json(categories);
+    }
 }
